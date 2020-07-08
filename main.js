fee39c2ac4ca76eda598b5e8c2d261f129ff38f9
Elena <elenadangelo00@gmail.com>
2020-07-09 00:09:24 +0200

    js desktop

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 $(document).ready(function () {
-
+  $('li.drop-item > a').mouseenter(function () {
+    $('.dropdown-lg').slideUp();
+    $(this).next('.dropdown-lg').slideDown();
+  });
+  $('.dropdown-lg').mouseleave(function () {
+    $('.dropdown-lg').slideUp();
+  });
 });
